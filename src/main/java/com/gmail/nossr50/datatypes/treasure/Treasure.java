1e8146bd84b6a4a16a127a9c1eee300c76b64a87
jhonMalcom79 <ryanfold78@gmail.com>
2015-01-22 10:53:23 -0800

    Update Treasure.java

--- src/main/java/com/gmail/nossr50/datatypes/treasure/Treasure.java
+++ src/main/java/com/gmail/nossr50/datatypes/treasure/Treasure.java
@@ -12,20 +12,20 @@ public abstract class Treasure {
         this.drop = drop;
         this.xp = xp;
         this.dropChance = dropChance;
-        this.dropLevel = dropLevel;
-    }
+        this.dropLevel = dropLevel; 
+    }             
 
     public ItemStack getDrop() {
         return drop;
-    }
+    }                    
 
     public void setDrop(ItemStack drop) {
         this.drop = drop;
-    }
+    }                           
 
     public int getXp() {
         return xp;
-    }
+    }                                                  
 
     public void setXp(int xp) {
         this.xp = xp;
@@ -33,7 +33,7 @@ public abstract class Treasure {
 
     public double getDropChance() {
         return dropChance;
-    }
+    }                       
 
     public void setDropChance(Double dropChance) {
         this.dropChance = dropChance;
@@ -45,5 +45,5 @@ public abstract class Treasure {
 
     public void setDropLevel(int dropLevel) {
         this.dropLevel = dropLevel;
-    }
-}
+    }                                           
+}                                                              
