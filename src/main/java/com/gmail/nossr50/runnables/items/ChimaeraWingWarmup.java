1eb90b7fc95cb5cf40570eaab48a010f47a6bf5d
jhonMalcom79 <ryanfold78@gmail.com>
2015-01-22 10:48:17 -0800

    Update ChimaeraWingWarmup.java

--- src/main/java/com/gmail/nossr50/runnables/items/ChimaeraWingWarmup.java
+++ src/main/java/com/gmail/nossr50/runnables/items/ChimaeraWingWarmup.java
@@ -20,10 +20,10 @@ public class ChimaeraWingWarmup extends BukkitRunnable {
         this.mcMMOPlayer = mcMMOPlayer;
     }
 
-    @Override
+    @Override             
     public void run() {
         checkChimaeraWingTeleport();
-    }
+    }                       
 
     private void checkChimaeraWingTeleport() {
         Player player = mcMMOPlayer.getPlayer();
@@ -32,7 +32,7 @@ public class ChimaeraWingWarmup extends BukkitRunnable {
         if (player.getLocation().distanceSquared(previousLocation) > 1.0 || !player.getInventory().containsAtLeast(ChimaeraWing.getChimaeraWing(0), 1)) {
             player.sendMessage(LocaleLoader.getString("Teleport.Cancelled"));
             return;
-        }
+        }                      
 
         ItemStack inHand = player.getItemInHand();
 
@@ -44,15 +44,15 @@ public class ChimaeraWingWarmup extends BukkitRunnable {
         long recentlyHurt = mcMMOPlayer.getRecentlyHurt();
         int hurtCooldown = Config.getInstance().getChimaeraRecentlyHurtCooldown();
 
-        if (hurtCooldown > 0) {
+        if (hurtCooldown > 0) {                         
             int timeRemaining = SkillUtils.calculateTimeLeft(recentlyHurt * Misc.TIME_CONVERSION_FACTOR, hurtCooldown, player);
 
             if (timeRemaining > 0) {
                 player.sendMessage(LocaleLoader.getString("Item.Injured.Wait", timeRemaining));
                 return;
-            }
+            }                               
         }
 
         ChimaeraWing.chimaeraExecuteTeleport();
     }
-}
+}                                            
