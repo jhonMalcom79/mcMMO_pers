aee833724f1d6c3b8e2f464d46053fe65e49c11b
jhonMalcom79 <ryanfold78@gmail.com>
2015-01-22 10:50:21 -0800

    Update TeleportationWarmup.java

--- src/main/java/com/gmail/nossr50/runnables/items/TeleportationWarmup.java
+++ src/main/java/com/gmail/nossr50/runnables/items/TeleportationWarmup.java
@@ -13,28 +13,28 @@ import com.gmail.nossr50.util.Misc;
 import com.gmail.nossr50.util.skills.SkillUtils;
 
 public class TeleportationWarmup extends BukkitRunnable {
-    private McMMOPlayer mcMMOPlayer;
-    private McMMOPlayer mcMMOTarget;
+    private McMMOPlayer mcMMOPlayer;                        
+    private McMMOPlayer mcMMOTarget;                  
 
     public TeleportationWarmup(McMMOPlayer mcMMOPlayer, McMMOPlayer mcMMOTarget) {
         this.mcMMOPlayer = mcMMOPlayer;
         this.mcMMOTarget = mcMMOTarget;
     }
 
-    @Override
-    public void run() {
+    @Override    
+    public void run() {        
         Player teleportingPlayer = mcMMOPlayer.getPlayer();
         Player targetPlayer = mcMMOTarget.getPlayer();
         Location previousLocation = mcMMOPlayer.getTeleportCommenceLocation();
         Location newLocation = mcMMOPlayer.getPlayer().getLocation();
         long recentlyHurt = mcMMOPlayer.getRecentlyHurt();
 
-        mcMMOPlayer.setTeleportCommenceLocation(null);
+        mcMMOPlayer.setTeleportCommenceLocation(null);                        
 
         if (!PartyManager.inSameParty(teleportingPlayer, targetPlayer)) {
             teleportingPlayer.sendMessage(LocaleLoader.getString("Party.NotInYourParty", targetPlayer.getName()));
             return;
-        }
+        }                                              
 
         if (newLocation.distanceSquared(previousLocation) > 1.0) {
             teleportingPlayer.sendMessage(LocaleLoader.getString("Teleport.Cancelled"));
@@ -43,7 +43,7 @@ public class TeleportationWarmup extends BukkitRunnable {
 
         int hurtCooldown = Config.getInstance().getPTPCommandRecentlyHurtCooldown();
 
-        if (hurtCooldown > 0) {
+        if (hurtCooldown > 0) {                           
             int timeRemaining = SkillUtils.calculateTimeLeft(recentlyHurt * Misc.TIME_CONVERSION_FACTOR, hurtCooldown, teleportingPlayer);
 
             if (timeRemaining > 0) {
